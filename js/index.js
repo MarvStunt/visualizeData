ce6d42dddf4ba4b914e84386fe016382717e33e5
MarvStunt <damien.conil@gmail.com>
2025-11-21 10:29:54 +0100

    Example of export function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
+// Import test function from compoentn/groupType.js
+import { test } from './components/groupType.js';
+
 $(document).ready(function() {
     // Load ajax components
     $('.hitmap-container').load('components/hitmap.html');
     $('.groupType-container').load('components/groupType.html');
     $('.weaponUsed-container').load('components/weaponUsed.html');
 });
+
+let dataCSV = d3.csv("./cleaned_data.csv");
+
+// Call function from groupType.js
+test(dataCSV);
