501e8b1aa8c0c0ce29c137c8ecaf1e90c43872ee
MarvStunt <damien.conil@gmail.com>
2025-11-21 10:02:46 +0100

    Begin project architecture

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Load ajax components
+    $('.hitmap-container').load('components/hitmap.html');
+    $('.groupType-container').load('components/groupType.html');
+    $('.weaponUsed-container').load('components/weaponUsed.html');
+});
